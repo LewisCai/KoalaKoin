52f9ef9bb511b19894a1c2168d06a392907f055e
Lewis20030124 <114333332+Lewis20030124@users.noreply.github.com>
2024-07-09 15:52:04 +1000

    done profile page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import Course from './components/Course';
 import Questions from './components/Questions';
+import Profile from './components/Profile';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="questions" element={<Questions />} />
         <Route path="course" element={<Course />} />
+        <Route path="profile" element={<Profile />} />
       </Route>
     </Routes>
     </>
--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -3,31 +3,44 @@ import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
 import LogoL from '../../assets/images/bluelogo.png';
 import LoginButton from '../LoginButton'; // Adjust the import path according to your project structure
+import { useAuth0 } from '@auth0/auth0-react';
 
-const Topbar = () => (
+const Topbar = () => {
+  const { isAuthenticated } = useAuth0();
+
+  return (
     <div className='nav-bar'>
-        <div className='left'>
-            <NavLink exact="true" activeclassname='active' className="nav-link" to='/course'>
-                Courses
-            </NavLink>
-            <div className="search-container">
-                <i className="fas fa-search search-icon"></i>
-                <input type="text" placeholder="Search" className="search-input" />
-            </div>
+      <div className='left'>
+        <NavLink exact="true" activeclassname='active' className="nav-link" to='/course'>
+          Courses
+        </NavLink>
+        <div className="search-container">
+          <i className="fas fa-search search-icon"></i>
+          <input type="text" placeholder="Search" className="search-input" />
         </div>
-        <div className='center'>
-            <Link className='logo' to='/'>
-                <img src={LogoL} alt="logo"/>
-            </Link>
-            <div className='brand-name'>Koala Koin</div>
-        </div>
-        <div className='right'>
-            <LoginButton />  {/* Use the LoginButton here */}
+      </div>
+      <div className='center'>
+        <Link className='logo' to='/'>
+          <img src={LogoL} alt="logo" />
+        </Link>
+        <div className='brand-name'>Koala Koin</div>
+      </div>
+      <div className='right'>
+        {isAuthenticated ? (
+          <NavLink exact="true" activeclassname='active' className="nav-link" to='/profile'>
+            Profile
+          </NavLink>
+        ) : (
+          <>
+            <LoginButton />
             <NavLink exact="true" activeclassname='active' className="nav-link signup" to='/signup'>
-                Sign up
+              Sign up
             </NavLink>
-        </div>
+          </>
+        )}
+      </div>
     </div>
-);
+  );
+};
 
 export default Topbar;
